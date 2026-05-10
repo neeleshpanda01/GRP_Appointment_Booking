ebea55ebe6fddbb996b677d644cff7f1042a828d
neeleshpanda01 <neelesh.kapil@gmail.com>
2026-05-10 21:18:10 +0530

    Completed auth backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/hospital");
+
+app.use("/api", require("./routes"));
+
+app.listen(5000, () => console.log("Server running on 5000"));
